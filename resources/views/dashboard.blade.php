211941a0f70faf61f2708d74fbe8a377ce4d7580
OgShas <shasog@gmail.com>
2025-08-04 18:31:24 +0300

    fix some staff

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,4 @@
+@php use Illuminate\Support\Facades\Auth; @endphp
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,36 +18,38 @@
 <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
     {{ __('Dashboard') }}
 </h2>
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                <table class="min-w-full bg-white">
-                    <thead>
-                    <tr>
-                        <th class="py-2 px-4 border-b">Order ID</th>
-                        <th class="py-2 px-4 border-b">Total Amount</th>
-                        <th class="py-2 px-4 border-b">Status</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($orders as $order)
+@if(Auth::check() && Auth::user()->is_admin)
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <table class="min-w-full bg-white">
+                        <thead>
                         <tr>
-                            <td class="py-2 px-4 border-b">{{$order->id}}</td>
-                            <td class="py-2 px-4 border-b">{{$order->total_amount}}</td>
-                            <td class="py-2 px-4 border-b">{{ucfirst($order->status)}}</td>
-                            <td class="py-2 px-4 border-b">
-                                <a href="#">Edit</a>
-                                <a href="#">Delete</a>
-                            </td>
+                            <th class="py-2 px-4 border-b">Order ID</th>
+                            <th class="py-2 px-4 border-b">Total Amount</th>
+                            <th class="py-2 px-4 border-b">Status</th>
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                        @foreach($orders as $order)
+                            <tr>
+                                <td class="py-2 px-4 border-b">{{$order->id}}</td>
+                                <td class="py-2 px-4 border-b">{{$order->total_amount}}</td>
+                                <td class="py-2 px-4 border-b">{{ucfirst($order->status)}}</td>
+                                <td class="py-2 px-4 border-b">
+                                    <a href="#">Edit</a>
+                                    <a href="#">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endif
 @if (Auth::check())
     <div class="container mt-4">
         <div class="d-flex flex-column gap-3 flex-wrap">
