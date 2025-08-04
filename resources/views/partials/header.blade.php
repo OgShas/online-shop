d641533869654af466e063020f4107c96bd672b3
OgShas <shasog@gmail.com>
2025-08-04 19:09:53 +0300

    use extended layouts

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,18 @@
 <header>
-    <div class="container-fluid d-flex justify-content-between align-items-center p-3 sticky-top shadow-sm bg-white mb-4">
+    <div
+        class="container-fluid d-flex justify-content-between align-items-center p-3 sticky-top shadow-sm bg-white mb-4">
         <a href="{{ url('/') }}" class="text-decoration-none">
-            <img src="{{ asset('images/logo.png') }}" alt="Shop Cars Logo" class="rounded" style="height: 50px; width: 50px"/>
+            <img src="{{ asset('images/logo.png') }}" alt="Shop Cars Logo" class="rounded"
+                 style="height: 50px; width: 50px"/>
         </a>
 
         @auth
             <div class="d-flex gap-3">
                 <a href="{{ url('/dashboard') }}" class="btn btn-outline-secondary">Dashboard</a>
                 <a href="{{ url('/products') }}" class="btn btn-outline-primary">Go to Products</a>
-                <a href="{{ url('/cart') }}" class="btn btn-outline-success">🛒 My Cart</a>
+                @if (!Auth::user()->role === 'admin')
+                    <a href="{{ url('/cart') }}" class="btn btn-outline-success">🛒 My Cart</a>
+                @endif
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
                     <button type="submit" class="btn btn-outline-danger">🔓 Log Out</button>
