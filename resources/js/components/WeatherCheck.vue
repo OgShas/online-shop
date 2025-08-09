ca3804e8ee1e5415122a2512ab8fa3480e093b56
OgShas <shasog@gmail.com>
2025-08-09 18:00:36 +0300

    use vue.js check weather complete

---
+++ resources/js/components/WeatherCheck.vue
@@ -0,0 +1,30 @@
+<template>
+    <section class="mb-5">
+        <h2 class="text-center mb-4">Check Weather by Country</h2>
+        <div class="d-flex justify-content-center">
+            <input type="text" v-model="city" placeholder="Enter country or city"
+                   class="form-control w-50 me-2">
+            <button @click="checkWeather" class="btn btn-primary">Check Weather</button>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'WeatherCheck',
+    data() {
+        return {
+            city: ''
+        }
+    },
+    methods: {
+        checkWeather() {
+            if (this.city.trim() === '') {
+                alert('Please enter a city or country');
+                return;
+            }
+            window.location.href = `/weather/${encodeURIComponent(this.city)}`;
+        }
+    }
+}
+</script>
